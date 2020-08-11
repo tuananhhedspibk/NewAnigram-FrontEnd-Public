57afeceecbfd1b12ff39e69ba89f77e1fecf3756
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2020-08-11 11:26:08 +0900

    Add route

---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+export const Routes = () => (
+  <Router history={history}>
+
+  </Router>
+);
