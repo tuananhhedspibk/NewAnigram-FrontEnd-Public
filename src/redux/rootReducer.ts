56443888cdc462b2f16bbf9ace43627732d94155
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2020-08-11 15:09:48 +0900

    Add notify reducer in rootReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './auth/reducer';
+import { notifyReducer } from './notify/reducer';
 
 export default combineReducers({
   auth: authReducer,
+  notify: notifyReducer,
 });
